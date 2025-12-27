196635ba7216d08aad595309efc8a2c5909a6df3
Wege0921 <wegemuluneh@gmail.com>
2025-12-28 00:40:22 +0300

    vercel and vite config

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  base: "/",
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
 
