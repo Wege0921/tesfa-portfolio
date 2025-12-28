48c07f75b9b0215e7b311cc2377635f4dad1a6fd
Wege0921 <wegemuluneh@gmail.com>
2025-12-28 11:42:52 +0300

    vite and vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from 'tailwindcss'
-// import autoprefixer from 'autoprefixer'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   css: {
-//     postcss: {
-//       plugins: [
-//         tailwindcss,
-//         autoprefixer,
-//       ],
-//     },
-//   },
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./",
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
